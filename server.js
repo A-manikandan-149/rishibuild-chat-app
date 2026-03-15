1e4ed55c4c41ded82e88b2f7bd9ee03c91ee3719
A-manikandan-149 <mk09200401@gmail.com>
2026-03-15 16:10:40 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -50,7 +50,6 @@ wss.on("connection", (ws) => {
 // Start server
 server.listen(3000, () => {
     console.log("🚀 Server running on port 3000");
-=======
 const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
